94bc1b623b91a0efb94c094fadea2815617be4b8
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-07-14 21:13:29 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+const msg = ref('Thrive vuejs')
+const todos = reactive([])
+
+function handleSubmit() {
+  // setData([...todos, msg.value])
+  todos.push(msg.value)
+  console.log(todos)
+}
+</script>
+
+<template>
+  <h1>Hello world!</h1>
+  <form @reset="msg = ''" @submit.prevent="handleSubmit">
+    <input v-model="msg" />
+    <button type="submit">Submit</button>
+    <button type="reset">Reset</button>
+  </form>
+
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index">
+      <span v-show="index < 3">
+        {{ todo }}
+      </span>
+    </li>
+  </ul>
+</template>
